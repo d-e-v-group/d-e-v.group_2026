dbfddac847e7efe78683c7a2c9814e53c2c72798
Brendan Sheehan <15129375+feemcgill@users.noreply.github.com>
2026-04-29 17:05:26 -0700

    adding something

---
+++ app/components/Marquee.vue
@@ -0,0 +1,29 @@
+<script setup>
+import content from '~/assets/js/content.js'
+
+const icons = [
+  'computer', 'documents', 'floppydisk', 'folder',
+  'mailbox', 'mouse', 'peace', 'pencil', 'thumbs-up',
+]
+
+const items = [...content.clients, ...content.clients]
+</script>
+
+<template>
+  <div class="marquee">
+    <div class="marquee-track">
+      <template v-for="(c, i) in items" :key="i">
+        <span>{{ c }}</span>
+        <span class="dot">
+          <img :src="`/icons/${icons[i % icons.length]}.svg`" alt="" aria-hidden="true" />
+        </span>
+      </template>
+      <template v-for="(c, i) in items" :key="`b${i}`">
+        <span>{{ c }}</span>
+        <span class="dot">
+          <img :src="`/icons/${icons[i % icons.length]}.svg`" alt="" aria-hidden="true" />
+        </span>
+      </template>
+    </div>
+  </div>
+</template>
